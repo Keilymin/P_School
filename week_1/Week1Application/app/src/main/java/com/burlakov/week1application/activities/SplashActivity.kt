02db4b2c860d7f470352d1f00ceca541bcf0ace8
Keilymin <drakono312@gmail.com>
2021-11-01 12:44:38 +0200

    Added coroutine for SplashActivity.kt

--- week_1/Week1Application/app/src/main/java/com/burlakov/week1application/activities/SplashActivity.kt
+++ week_1/Week1Application/app/src/main/java/com/burlakov/week1application/activities/SplashActivity.kt
@@ -3,13 +3,21 @@ package com.burlakov.week1application.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.lang.Thread.sleep
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        sleep(1000)
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+
+        CoroutineScope(Dispatchers.Default).launch{
+            delay(1000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+
     }
 }
